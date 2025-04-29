245112297ee175c3cc926e82228e2afe49584171
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    raphael: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,6 @@ TARGET_OTA_ASSERT_DEVICE := raphael,raphaelin
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_raphael
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_raphael
 
 # Kernel
--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_raphael)
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
