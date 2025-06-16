23c20c08ba70651be75e0c56f2eb21eb57ab9e53
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    raphael: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -23,7 +23,10 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(
             b'/vendor/lib/liba2dpoffload.so',
             b'liba2dpoffload_raphael.so\x00\x00\x00\x00',
-        ),
+        )
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
 }  # fmt: skip
