LineageOS/android_device_xiaomi_cepheus

raphael: Migrate to py extract utils
Change-Id: I70f8b5bb9322a4ffca21bebe9d9b47fb934c8a79

extract-files.py
@@ -1,0 +1,51 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8150',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
+    'vendor/xiaomi/sm8150-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
+    'vendor/lib/hw/audio.primary.cepheus.so': blob_fixup()
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_cepheus.so\x00\x00\x00\x00',
+        ),
+    'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'cepheus',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8150-common', module.vendor
+    )
+    utils.run()

LineageOS/android_device_xiaomi_raphael

extract-files.py
@@ -1,0 +1,51 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
+    'vendor/lib/hw/audio.primary.raphael.so': blob_fixup()
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_raphael.so\x00\x00\x00\x00',
+        ),
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8150',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
+    'vendor/xiaomi/sm8150-common',
+]
+
+module = ExtractUtilsModule(
+    'raphael',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8150-common', module.vendor
+    )
+    utils.run()